0201012a54285c9851dfe8b23045f0d99a8175cd
Ryan Maxwell <ryanm@xwell.co.nz>
2015-01-04 22:47:31 +1300

    Add in UIKit import into header.

--- RMUniversalAlert.h
+++ RMUniversalAlert.h
@@ -6,6 +6,8 @@
 //  Copyright (c) 2014 Ryan Maxwell. All rights reserved.
 //
 
+#import <UIKit/UIKit.h>
+
 @class RMUniversalAlert;
 
 typedef void(^RMUniversalAlertCompletionBlock)(RMUniversalAlert *alert, NSInteger buttonIndex);
